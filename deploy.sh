f7d5309874a070913ba96e3179d7862e33f65727
Nikhil Uday Kumar <nikhil.uday@cohesity.com@ML504N964GG.community.veritas.com>
2026-01-09 15:57:49 +0530

    Fix PEP 668 issue using Python virtualenv

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 set -e
 
 APP_DIR=/home/ubuntu/flaskapp
+VENV_DIR=$APP_DIR/venv
 
-echo "Creating app directory..."
+echo "Preparing app directory..."
 rm -rf $APP_DIR
 mkdir -p $APP_DIR
 
@@ -12,8 +13,15 @@ cp app.py requirements.txt $APP_DIR/
 
 cd $APP_DIR
 
-echo "Installing dependencies..."
-pip3 install -r requirements.txt
+echo "Creating virtual environment..."
+python3 -m venv venv
+
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+echo "Installing dependencies inside venv..."
+pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "Restarting application..."
 sudo systemctl daemon-reload
